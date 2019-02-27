ef0ea045d4c023fe40c4036cad2d3927a2dcafc9
Jennifer Rubin <jenlrubin9326@gmail.com>
2019-02-27 13:55:07 -0500

    many file changes

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -7,13 +7,31 @@ $(".smooth").on("click", function() {
   return false;
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".fixed-action-btn");
-  var instances = M.FloatingActionButton.init(elems, options);
-});
-
-// Or with jQuery
-
 $(document).ready(function() {
   $(".fixed-action-btn").floatingActionButton();
+});
+
+function typeEffect(element, speed) {
+  var text = $(element).text();
+  $(element).html("");
+
+  var i = 0;
+  var timer = setInterval(function() {
+    if (i < text.length) {
+      $(element).append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+$(document).ready(function() {
+  var speed = 150;
+  var delay = $(".landingPage-text").text().length * speed + speed;
+  typeEffect($(".landingPage-text"), speed);
+  setTimeout(function() {
+    $(".info").css("display", "inline-block");
+    typeEffect($(".info"), speed);
+  }, delay);
 });
